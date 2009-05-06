91f6310dcca60a8ded101b91b7833392965c0110
Andrey Levushkin <andrey.levushkin@gmail.com>
2009-05-06 19:50:36 +0100

    Added javascript to prevent page caching of the DiscussionPage.

--- src/main/webapp/js/misc.js
+++ src/main/webapp/js/misc.js
@@ -1,23 +1,32 @@
-function showLocalNav(){
-	$('local_home_nav').style.display="block";
+function showLocalNav() {
+	$('local_home_nav').style.display = "block";
 }
 
-function hideLocalNav(){
-	$('local_home_nav').style.display="none";
+function hideLocalNav() {
+	$('local_home_nav').style.display = "none";
 }
 
-function showAdminOptions(){
-	$('admin_options').style.display="block";
+function showAdminOptions() {
+	$('admin_options').style.display = "block";
 }
 
-function hideAdminOptions(){
-	$('admin_options').style.display="none";
+function hideAdminOptions() {
+	$('admin_options').style.display = "none";
 }
 
-function refreshHack(){
-	var url=window.document.URL;
-	var lastChar=url.charAt(url.length-1);
-	if(lastChar=="#"){
-		 window.location.replace( url.substring(0,url.length-2) );
+/**
+ * This functions should be called in <body onUnload=""/> it makes sure that
+ * when the user hists the back button he hoes not see the cached version of the
+ * page. This ensures that the users search of page is persisted when the user
+ * clicks back out of the details page. Its a bit of a hack.
+ * 
+ * @return
+ */
+function refreshHack() {
+	try {
+		var headElement = document.getElementsByTagName("head")[0];
+		if (headElement && headElement.innerHTML)
+			headElement.innerHTML += "<meta http-equiv=\"refresh\" content=\"1\">";
+	} catch (e) {
 	}
 }
